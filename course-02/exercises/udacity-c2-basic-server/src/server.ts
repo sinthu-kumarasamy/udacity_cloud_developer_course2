434c1baefbfcc03ddad3bdcbbf91bf0a060152fc
Sinthu <kumarasamy.s@husky.neu.edu>
2020-06-15 00:23:56 -0400

    Completed exercises

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -17,7 +17,7 @@ import { Car, cars as cars_list } from './cars';
 
   // Root URI call
   app.get( "/", ( req: Request, res: Response ) => {
-    res.status(200).send("Welcome to the Cloud!");
+    res.status(200).send("Welcome to the test!");
   } );
 
   // Get a greeting to a specific person 
@@ -70,13 +70,55 @@ import { Car, cars as cars_list } from './cars';
 
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
+  app.get( "/cars/", ( req: Request, res: Response ) => {
+    let {make} = req.query;
+    
+    let car_list = cars
+
+    if ( make) {
+        car_list = cars.filter((car) =>car.make === make)
+    }
+
+    return res.status(200)
+              .send(car_list);
+  } );
 
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get( "/cars/:id", ( req: Request, res: Response ) => {
+    let {id} = req.params;
+    
+    let car_list = cars.filter((car) =>car.id == id)
+    if (car_list.length === 0) {
+      return res.status(404)
+      .send('Id not found');
+    }
+
+    return res.status(200)
+              .send(car_list);
+  } );
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post( "/cars/",  async ( req: Request, res: Response ) => {
+      let { make,type,model,cost,id } = req.body;
+      if ( !id || !type || !model || !cost ) {
+        return res.status(400)
+                  .send(`Please fill all fields`);
+      }
+      const car:Car ={
+        make:make,
+        type:type,
+        model:model,
+        cost:cost,
+        id : id,
+      }
+      cars.push(car)
+      return res.status(201)
+                .send(cars);
+  } );
+
 
   // Start the Server
   app.listen( port, () => {
